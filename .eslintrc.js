a4db6990df174d966c86e807140be5bfa62f95eb
Tomer <tomer.steinfeld@applitools.com>
2018-04-24 16:10:57 +0300

    eslint jest env

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   "env": {
     "browser": true,
     "node": true,
-    "es6": true
+    "es6": true,
+    "jest": true
   },
   "extends": ["eslint:recommended"],
   "parser": "babel-eslint",
